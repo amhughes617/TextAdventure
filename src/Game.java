cbacb9133a4f8d53cf8bec07a459fcfe5b990651
Alex <amhughes@g.cofc.edu>
2016-02-03 11:59:09 -0500

    Refactored code into player class

--- src/Game.java
+++ src/Game.java
@@ -6,40 +6,15 @@ import java.util.logging.FileHandler;
  * Created by alexanderhughes on 2/3/16.
  */
 public class Game {
+    public static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) throws Exception {
         System.out.println("Welcome, traveller.");
-        System.out.println("What is your name");
+        Player player = new Player();
+        player.chooseName();
+        player.chooseWeapon();
+        player.chooseLocation();
 
-        Scanner scanner = new Scanner(System.in);
-
-        String name = scanner.nextLine();
-        System.out.println("Welcome, " + name);
-
-        System.out.println("Choose your weapon [sword/mace]");
-        String weapon = scanner.nextLine();
-
-        if(weapon.equalsIgnoreCase("sword")) {
-            System.out.println("A sword is a fine choice!");
-        }
-        else if (weapon.equalsIgnoreCase("mace")) {
-            System.out.println("A mace is a fine choice!");
-        }
-        else {
-            throw new Exception("Invalid weapon!");         // must declare this exception at the top of the method
-        }
-
-        System.out.println("Choose your location [forest/tunnel]");
-        String location = scanner.nextLine();
-
-        if (location.equalsIgnoreCase("forest")) {
-            System.out.println("Entering forest...");
-        }
-        else if (location.equalsIgnoreCase("tunnel")) {
-            System.out.println("Entering tunnel...");
-        }
-        else {
-            throw new Exception("Invalid Location!");
-        }
     }
 
 }
